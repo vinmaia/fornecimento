a25ffdd58aa95e948d0f5b14379828890678a0a2
Maia <vincecake008@gmail.com>
2024-09-02 10:04:10 -0300

    feat: models

---
+++ src/models/mercado.js
@@ -0,0 +1,21 @@
+/*Mercado:
+ - nome
+ - endereco 
+ */
+
+let proxId = 1;
+
+const model = (body, id = proxId++) => {
+  if (
+    body.nome != undefined &&
+    body.nome != ""
+  ) {
+    return {
+      id,
+      nome: body.nome,
+      endereco: body.endereco,
+    };
+  }
+};
+
+module.exports = model;
